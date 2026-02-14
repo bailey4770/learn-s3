a282cb6105dcc8831c36a1b42488455d6ed8da07
Bailey Stringer <bailey4770@protonmail.com>
2026-02-14 16:30:32 +0000

    refactor(thumbnails): save image data in assetsRoot rather than db

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,10 +1,13 @@
 package main
 
 import (
-	"encoding/base64"
 	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
@@ -25,7 +28,7 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	log.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	mediaType, imageData, err := parseThumbnailReq(w, r)
+	mediaType, multipartFile, err := parseThumbnailReq(w, r)
 	if err != nil {
 		log.Println("Error: could not parse request:", err)
 		return
@@ -41,7 +44,13 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	}
 	log.Println("Info: image metdata retrieved from db and user ID verified")
 
-	cfg.updateThumbnail(videoID, imageData, mediaType, metadata)
+	if err := cfg.updateThumbnail(videoID, multipartFile, mediaType, metadata); err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update thumbnail", err)
+		log.Println("Error: could not update thumbnail:", err)
+		return
+	}
+
+	log.Println("Info: thumbnail successfully set")
 	respondWithJSON(w, http.StatusOK, metadata)
 }
 
@@ -68,35 +77,41 @@ func validateRequest(cfg *apiConfig, w http.ResponseWriter, r *http.Request) (uu
 	return videoID, userID, nil
 }
 
-func parseThumbnailReq(w http.ResponseWriter, r *http.Request) (string, []byte, error) {
+func parseThumbnailReq(w http.ResponseWriter, r *http.Request) (string, multipart.File, error) {
 	if err := r.ParseMultipartForm(maxMemory); err != nil {
 		respondWithError(w, http.StatusBadRequest, "Couldn't parse multipart form", err)
 		return "", nil, err
 	}
 
-	file, header, err := r.FormFile("thumbnail")
+	multipartFile, header, err := r.FormFile("thumbnail")
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't form file the request", err)
 		return "", nil, err
 	}
 
-	mediaType := header.Header.Get("Content-Type")
+	contentType := strings.Split(header.Header.Get("Content-Type"), "/")
+	mediaType := contentType[1]
 
-	imageData, err := io.ReadAll(file)
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Couldn't read image data", err)
-		return "", nil, err
-	}
-
-	return mediaType, imageData, nil
+	return mediaType, multipartFile, nil
 }
 
-func (cfg *apiConfig) updateThumbnail(videoID uuid.UUID, imageData []byte, mediaType string, metadata database.Video) {
-	encoded := base64.StdEncoding.EncodeToString(imageData)
-	tnURL := "data:" + mediaType + ";base64," + encoded
+func (cfg *apiConfig) updateThumbnail(videoID uuid.UUID, multipartFile multipart.File, mediaType string, metadata database.Video) error {
+	fileName := videoID.String() + "." + mediaType
+	thumbnailFilePath := filepath.Join(cfg.assetsRoot, fileName)
 
+	file, err := os.Create(thumbnailFilePath)
+	if err != nil {
+		return err
+	}
+
+	if _, err = io.Copy(file, multipartFile); err != nil {
+		return err
+	}
+
+	tnURL := "http://localhost:" + cfg.port + "/assets/" + fileName
 	metadata.ThumbnailURL = &tnURL
 	cfg.db.UpdateVideo(metadata)
 
 	log.Println("Info: thumbnail URL updated in db")
+	return nil
 }
